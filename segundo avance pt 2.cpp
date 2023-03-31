100311dc3098d0e1d3580c1377f82154962bbf60
leonelgzz <129493518+leonelgzz@users.noreply.github.com>
2023-03-31 01:50:25 -0600

    segundo avance Leonel Gonzalez

---
+++ segundo avance pt 2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	do {
+		int a, b;
+
+		cout << "\t Bienvenidos a la tienda \n" << endl;
+		cout << "1-Alta de videojuegos \n" << endl;
+		cout << "2-Modificacion de alta de videojuego \n" << endl;
+		cout << "3-Baja de videojuegos \n" << endl;
+		cout << "4-Lista de videojuegos \n" << endl;
+		cout << "5-limpiar pantalla\n " << endl;
+		cout << "6-Salir\n " << endl;
+		cin >> a;
+
+		switch (a)
+		{
+		case 1:
+			cout << "El juego disponible es un cheto vs un dorito,lo quieres?";
+			cout << "\n 1- jalado" << endl;
+			cout << "\n 2- No pa \n";
+			cin >> b;
+			if (b = 1);
+		case 4:
+			cout << "\n 1-Aqui tienes una lista de los videojuegos" << endl;
+			cout << "\n 2-doritos vs chetos" << endl;
+			cout << "\n 3-la venganza de pochita" << endl;
+			cout << "\n 4-Nacho vs pochita,quien se queda con su mama?" << endl;
+
+
+		case 3:
+			cout << "Baja de Videojuegos\n" << endl;
+
+		case 6:
+
+		
+
+
+
+
